906b18b02bd3c66042156826b86f4180145aef6b
Shvang42 <Shivang.patel961@gmail.com>
2024-01-28 11:36:21 -0500

    some changes

--- Avalon/views.py
+++ Avalon/views.py
@@ -8,5 +8,5 @@ def enter_username(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         request.session['username'] = username
-        return redirect('home')  # Redirect to your home page or other page
+        return redirect('active_sessions')  # Redirect to your home page or other page
     return render(request, 'enter_username.html')
