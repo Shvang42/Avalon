b9333dc9aa8aec2945393a982aea8fd91f30a827
Shvang42 <Shivang.patel961@gmail.com>
2024-03-03 13:15:16 -0500

    dashboard

--- Avalon/models.py
+++ Avalon/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
     password = models.CharField(max_length=30)
     date = models.DateTimeField(auto_now=True)
     lobby_id = models.CharField(max_length=50, null=True)
+    card_id = models.IntegerField(max_length=2, null=True)
 
 class Lobby(models.Model):
     lobby_name = models.CharField(max_length=30)
@@ -19,3 +20,9 @@ class Lobby(models.Model):
 
     def __str__(self):
         return f"{self.lobby_name} {self.lobby_code} {self.lobby_users} {self.lobby_host} {self.lobby_date}"
+    
+class Card(models.Model):
+    card_name = models.CharField(max_length=30, primary_key=True, unique=True)
+    card_num = models.IntegerField(max_length=2)
+    card_description = models.CharField(max_length=100)
+    card_color = models.BooleanField(empty_strings_allowed=False)
